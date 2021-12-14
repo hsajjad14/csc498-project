3ea3f7851cdde952aa3299749c60f227a8d6cea1
DuoDuoCai <84486708+duoduocai-dot@users.noreply.github.com>
2021-12-14 10:11:20 -0500

    improve the model

--- Breakout/dqn1.py
+++ Breakout/dqn1.py
@@ -1,6 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import gym
+# import matplotlib.pyplot as plt
 import torch
 from torch import nn
 from breakout import Breakout
--- Breakout/drqn.py
+++ Breakout/drqn.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import gym
+# import gym
 import torch
 from torch import nn
 from breakout import Breakout
